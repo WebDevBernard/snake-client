febd3398746bc3958dac8fe1ef81d20b77b64ed2
Bernard Yang <ybernard@gmail.com>
2021-06-02 20:49:27 +0000

    added initials in client reply conn.write

--- client.js
+++ client.js
@@ -9,7 +9,11 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  // connect
+  conn.on("connect", () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: BY');
+  });
   conn.on("data", (data) => {
     console.log('server says:', data);
   });
